61103fb0d5f181ba025e937e11d351a8fb3dbbff
235 <admin@bogon.intra.legendsec.com>
2017-11-28 17:05:48 +0800

    更新百度关键字搜索

--- baiducrawler.py
+++ baiducrawler.py
@@ -1,4 +1,8 @@
-import requests,re,math,sys
+import requests
+import re
+import math
+import sys
+import time
 from bs4 import BeautifulSoup
 headers = { "Accept":"text/html,application/xhtml+xml,application/xml;",
             "Accept-Encoding":"gzip",
@@ -11,9 +15,9 @@ def get_pagenum(url,site=False):
 		req = requests.get(url,headers=headers)
 		text = req.text
 		if site:
-			r = re.search(r"该网站共有(.*?)个网页被百度收录")
+			r = re.search(r"<b style=\"color:#333\">(.*?)<\/b>",text)
 		else:
-			r = re.search(r"百度为您找到相关结果约(.*?)个",text)
+			r = re.search(r"找到相关结果约(.*?)个",text)
 		if r:
 			num = r.group(1)
 		else:
@@ -21,24 +25,27 @@ def get_pagenum(url,site=False):
 		num = int(num.replace(",",""))
 		return(num)
 	except Exception as e:
-		print("Error: %s" , e)
+		print(e)
 		return 0
-def set_keyword(keyword,site=False):
-	if site:
-		http = "https://www.baidu.com/s?wd=site:%s&ie=utf-8" % keyword
-	else:
-		http = "https://www.baidu.com/s?wd=%s&ie=utf-8" % keyword
+def set_keyword(keyword,site=False,gpc="0"):
+	http = "https://www.baidu.com/s?wd=%s&gpc=%s&ie=utf-8" % (keyword,gpc)
 	return http
-def get_urls(keyword,site=False):
+def search(keyword,site=False,gpc="0"):
+	"""gpc为搜索时间参数"""
 	urls=[]
 	if keyword.find('site:') > -1:
 		site = True
-	url = set_keyword(keyword,site)
+	if gpc == 'd':
+		t = int(time.time())
+		tt = t - 86400
+		gpc = "stf%%3D%s%%2C%s%%7Cstftype%%3D1" % (tt,t)
+		site = False
+	url = set_keyword(keyword,site,gpc)
 	num = get_pagenum(url,site)
 	num_mix = 0
 	num_max = 75 if num/10 > 75 else math.ceil(num/10)
-	print(num_max)
-	for i in range(num_mix,num_max):
+	print("发现%s页" % num_max)
+	for i in range(num_max):
 		print("开始爬第%s页" % (i+1))
 		http = url + "&pn=%s" % (i*10)
 		req = requests.get(http,headers=headers)
@@ -49,63 +56,9 @@ def get_urls(keyword,site=False):
 			req = requests.get(b["href"],headers=headers,allow_redirects = False)
 			text = req.headers["Location"]
 			urls.append(text)
-		print("总共爬取%s条" % (len(urls)))
-		return urls
-
-print(get_urls("sinosig.com"))
-
-import requests,re,math
-from bs4 import BeautifulSoup
-headers = { "Accept":"text/html,application/xhtml+xml,application/xml;",
-            "Accept-Encoding":"gzip",
-            "Accept-Language":"zh-CN,zh;q=0.8",
-            "Referer":"https://www.baidu.com/",
-            "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
-            }
-def get_pagenum(url,site=False):
-	try:
-		req = requests.get(url,headers=headers)
-		text = req.text
-		if site:
-			r = re.search(r"该网站共有(.*?)个网页被百度收录")
-		else:
-			r = re.search(r"百度为您找到相关结果约(.*?)个",text)
-		if r:
-			num = r.group(1)
-		else:
-			num = '0'
-		num = int(num.replace(",",""))
-		return(num)
-	except Exception as e:
-		print("Error: %s" , e)
-		return 0
-def set_keyword(keyword,site=False):
-	if site:
-		http = "https://www.baidu.com/s?wd=site:%s&ie=utf-8" % keyword
-	else:
-		http = "https://www.baidu.com/s?wd=%s&ie=utf-8" % keyword
-	return http
-def get_urls(keyword,site=False):
-	urls=[]
-	if keyword.find('site:') > -1:
-		site = True
-	url = set_keyword(keyword,site)
-	num = get_pagenum(url,site)
-	num_mix = 0
-	num_max = 75 if num/10 > 75 else math.ceil(num/10)
-	print(num_max)
-	for i in range(num_mix,num_max):
-		print("开始爬第%s页" % (i+1))
-		http = url + "&pn=%s" % (i*10)
-		req = requests.get(http,headers=headers)
-		text = req.text
-		soup = BeautifulSoup(text,"html.parser")
-		b_list = soup.find_all("a",class_="c-showurl") #baidu 跳转地址
-		for b in b_list:
-			req = requests.get(b["href"],headers=headers,allow_redirects = False)
-			text = req.headers["Location"]
-			urls.append(text)
-		print("总共爬取%s条" % (len(urls)))
-		return urls
-
-print(get_urls(sys.argv[1]))
+	print("总共爬取%s条" % (len(urls)))
+	return urls
+if len(sys.argv) > 2:
+	print(search(sys.argv[1],gpc=sys.argv[2]))
+else:
+	print(search(sys.argv[1]))

067b8951222905bf20ede5e0faffb33433bb38e4
Test <Test@gmail.com>
2022-06-24 10:54:31 +0800

    nmpSearch used async

--- npmSearch.py
+++ npmSearch.py
@@ -1,45 +1,67 @@
-#npm keywrods search
-import requests
+import aiohttp
+from aiofile import async_open
 import json
+import asyncio
 import sys
-
 npmSearchApi = "https://npmmirror.com/package/{keyword}"
+async def checkPackage(keyword):
+    url = npmSearchApi.format(keyword=keyword)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url) as req:
+            status_code = req.status
+            if status_code != 200:
+                print(keyword,"不存在")
+    
+async def getPackageByUrl(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as req:
+            if req.status ==200:
+                return json.loads(await req.text())
+        return False
 
-def checkPackage(keyword):
-	req = requests.get(url=npmSearchApi.format(keyword=keyword.strip()))
-	if req.status_code != 200:
-		print(keyword,"不存在")
+async def getPackageByFile(filepath):
+    async with async_open(filepath,"r") as fhand:
+        text = await fhand.read()
+        return json.loads(text)
 
-def getPackageByUrl(url):
-	req = requests.get(url)
-	if req.status_code == 200:
-		return req.json()
-	return False
+async def asyncList(slist):
+    __tmpArray = []
+    listSize = len(slist)
+    listIndex = 0
+    for k,v in slist.items():
+        __tmpArray.append(k)
+    while True:
+       if listIndex > listSize - 1: 
+           break
+       yield __tmpArray[listIndex]
+       listIndex = listIndex + 1
 
-def getPackageByFile(filepath):
-	filehandle = open(filepath,"r")
-	jsonstr = json.loads(filehandle.read())
-	filehandle.close()
-	return jsonstr
 
 def printHelp():
-	print(sys.argv[0] + " --url https://github.com/github/package.json")
-	print(sys.argv[0] + " --file ./package.json")
+    print(sys.argv[0] + " --url https://github.com/github/package.json")
+    print(sys.argv[0] + " --file ./package.json")
+
+async def main():
+    #async with aiohttp.ClientSession() as session:
+    if "--url" in sys.argv:
+        packageJson = await getPackageByUrl(sys.argv[2])
+    elif "--file" in sys.argv:
+        packageJson = await getPackageByFile(sys.argv[2])
+    else:
+        printHelp()
+        exit()
+    packageList = {}
+    packageList.update(packageJson.get("dependencies",{}))
+    packageList.update(packageJson.get("devDependencies",{}))
+    if len(packageList) == 0:
+        exit()
+    print(packageJson.get("repository",{}).get("url",""))
+    async for p in asyncList(packageJson):
+        await checkPackage(p)
+        
+
 if __name__ == "__main__":
-	if "--url" in sys.argv:
-		packageJson = getPackageByUrl(sys.argv[2])
-	elif "--file" in sys.argv:
-		packageJson = getPackageByFile(sys.argv[2])
-	else:
-		printHelp()
-		exit()
-	packageList = {}
-	packageList.update(packageJson.get("dependencies",{}))
-	packageList.update(packageJson.get("devDependencies",{}))
-	if len(packageList) == 0:
-		exit()
-	print(packageJson.get("repository").get("url"))
-	for p,v in packageList.items():
-		checkPackage(p)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
 
-
+    

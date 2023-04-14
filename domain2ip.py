e77b8ece6e6e63196e7c6039e24837d996a48330
Test <Test@gmail.com>
2023-04-14 17:44:59 +0800

    update domain2ip.py

--- domain2ip.py
+++ domain2ip.py
@@ -1,8 +1,7 @@
 import socket
 import sys
 import time
-def domain2ip(domain_file):
-    domains = open(domain_file,'r').readlines()
+def domain2ip(domains):
     for domain in domains:
         domain = domain.strip()
         try:
@@ -11,5 +10,18 @@ def domain2ip(domain_file):
             ip = '127.0.0.1'
         print(ip,domain)
 
+
+
 if __name__ == '__main__':
-    domain2ip(sys.argv[1])
+    if sys.argv[1] == '--stdin':
+        domains = sys.stdin.read().split("\n")
+        domain2ip(domains)
+
+
+    else:
+        domain_file = sys.argv[1]
+        domains = open(domain_file,'r').readlines()
+        domain2ip(domains)
+
+        
+
